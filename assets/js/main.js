983da283ccfe225fc4dd5c2a1c3d6ad5d68d8787
vuk480 <78022961+vuk480@users.noreply.github.com>
2024-03-13 01:36:50 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function() {
 $('.sliderMain-slider').slick({
   slidesToShow:3.5,
@@ -8,8 +9,7 @@ $('.sliderMain-slider').slick({
   dots: false,
   adaptiveHeight: true,
   
-
- // slider responsivo 
+ 
   responsive: [
   {
       breakpoint: 1121,
@@ -49,103 +49,15 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", 
 }));
 
 
-// var katReq = new XMLHttpRequest();
-// katReq.open('GET', '/assets/js/kategorije.json');
-// katReq.onload = function() {
-//   if (katReq.status >= 200 && katReq.status < 400) {
-//     // This is where we'll do something with the retrieved data
-//     var data = JSON.parse(katReq.responseText);
-    
-//    // createHTML(data);
-//   } else {
-//     console.log("We connected to the server, but it returned an error.");
-//   }
-// };
-
-// katReq.onerror = function() {
-//   console.log("Connection error");
-// };
-
-// katReq.send();
-
-
-
-
-
-var proba = document.getElementById('proba');
-
-var test = document.getElementById('myDiv');
-
-var test1 = test.getAttribute('data-custom');
-
-var temp="";
-
-proba.addEventListener('mouseenter',()=>{
-  var niktoni = document.getElementById('test11');
-  temp = niktoni.getAttribute('src');
-  niktoni.removeAttribute('src');
-  niktoni.setAttribute('src',test1);
-})
-
-var temp2="";
-
-proba.addEventListener('mouseleave',()=>{
-  var niktoni = document.getElementById('test11');
-  temp2 = niktoni.getAttribute('src');
-  niktoni.removeAttribute('src');
-  niktoni.setAttribute('src',temp);
-})
-
+var proba = localStorage.getItem('brItemaUKorpi');
+console.log(proba)
+    if(proba !== null || proba !== undefined || proba.length !== 0){
+    document.getElementById('numbItems').innerHTML = proba
+  }else{
+    document.getElementById('numbItems').innerHTML = "";
+  }
 
 })
-
-
-var nesto="";
-function print_shoe_detail(){
-  
-  var shoeName = JSON.parse(localStorage.getItem('name'));
-  var shoeGender = JSON.parse(localStorage.getItem('gender'));
-  var shoePrice = JSON.parse(localStorage.getItem('price'));
-  var img1 = (localStorage.getItem('img1'));
-  var img2 = JSON.parse(localStorage.getItem('img2'));
-  var img3 = JSON.parse(localStorage.getItem('img3'));
-  
-
-  // Update the elements with retrieved data
-  document.getElementById('shoe-name').innerHTML = shoeName;
-  document.getElementById('shoe-gender').innerHTML = shoeGender;
-  document.getElementById('shoe-price').innerHTML = shoePrice;
-  document.getElementById('main-photo-img').src = img1;
-  document.getElementById('photo1-3').src = img1;
-  document.getElementById('photo2-3').src = img2;
-  document.getElementById('photo3-3').src = img3;
-
-
-}
-
-
-function getValues(val) {
- 
-
-  var name = document.getElementsByName('shoeName')[val].value;
-  localStorage.setItem("name",name);
-  console.log(localStorage.getItem('name'));
-  var gender = document.getElementsByName('shoeGender')[val].value; // Access the first element
-  localStorage.setItem("gender", gender);
-  var price = document.getElementsByName('shoePrice')[val].value;
-  localStorage.setItem("price", price);
-  var img1 = document.getElementsByName('img1')[val].value;
-  localStorage.setItem("img1", img1);
-  var img2 = document.getElementsByName('img2')[val].value;
-  localStorage.setItem("img2", img2);
-  var img3 = document.getElementsByName('img3')[val].value;
-  localStorage.setItem("img3", img3);
-
-  print_shoe_detail();
-
-
-}
-
 
 var aboutCon=[
   "Fueled by our shared passion for sneakers and frustration at inflated resale prices, Sarah and I launched Sole Mates. We started small, leveraging our combined knowledge to source the hottest releases. Transparency was key, so we built our brand on clear communication, fair pricing, and a love for the sneaker community. Running a business with a friend presented challenges, but our shared vision and trust kept us pushing forward. Sole Mates has grown, attracting a loyal base that appreciates our dedication and fostering creativity through collaborations. It's been an incredible journey, solidifying our friendship through our shared passion."
@@ -159,16 +71,16 @@ function proveraSign() {
   if (regEmail.test(emailVal)) {
       var err = document.querySelector('.msg-code');
 
-      // Check if a discount code is already assigned to the email
+     
       var storedCode = localStorage.getItem(emailVal);
 
       if (storedCode) {
           err.innerHTML = "Your discount code is: " + storedCode;
       } else {
-          // Generate a new discount code
+         
           var code = Math.floor(Math.random() * 90000) + 10000;
 
-          // Store the discount code in localStorage using the email as the key
+         
           localStorage.setItem(emailVal, code);
 
           err.innerHTML = "Your discount code is: " + code;
